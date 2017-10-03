d48ab25d0694cf4f96e2506217b89e7bda07eb71
Chabane <chabane.refes@gmail.com>
2017-10-03 19:55:29 +0200

    style: remove unused dependency

--- src/main/scala/com/mitosis/config/ConfigurationFactory.scala
+++ src/main/scala/com/mitosis/config/ConfigurationFactory.scala
@@ -1,6 +1,5 @@
 package com.mitosis.config
 
-import com.google.common.base.Strings
 import com.mitosis.config.objects.Config
 import com.typesafe.config.ConfigBeanFactory
 import com.typesafe.config.ConfigFactory
